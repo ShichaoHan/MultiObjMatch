55d71ae619be6b68363cab2191c96d16c8290a5b
Shichao Han <schan21@berkeley.edu>
2020-12-13 00:03:20 -0800

    new changes to some files

--- R/dist_funcs.R
+++ R/dist_funcs.R
@@ -1,9 +1,9 @@
 require(fields)
 build.dist.struct<-
   function(z, X, exact = NULL, dist.type = "Mahalanobis", calip.option = 'propensity', calip.cov = NULL, caliper = 0.2, verbose = FALSE){
-    
+
     cal.penalty <- 100
-    if(is.null(exact)) exact = rep(1, length(z))	
+    if(is.null(exact)) exact = rep(1, length(z))
     if(!(calip.option %in% c('propensity','user','none'))){
       stop('Invalid calip.option specified.')
     }
@@ -17,7 +17,7 @@ build.dist.struct<-
     if(!(all((z == 1) | (z == 0)))){
       stop("The z argument must contain only 1s and 0s")
     }
-    
+
     if(is.data.frame(X) || is.character(X)){
       if(!is.data.frame(X)) X <- as.data.frame(X)
       X.chars <- which(laply(X, function(y) 'character' %in% class(y)))
@@ -25,12 +25,12 @@ build.dist.struct<-
         if (verbose) print('Character variables found in X, converting to factors.')
         for(i in X.chars){
           X[,i] <- factor(X[,i])
-          
+
         }
       }
       #if some variables are factors convert to dummies
       X.factors <-  which(laply(X, function(y) 'factor' %in% class(y)))
-      
+
       #handle missing data
       for(i in which(laply(X, function(x) any(is.na(x))))){
         if (verbose) print(paste('Missing values found in column', i ,'of X; imputing and adding missingness indicators'))
@@ -48,27 +48,27 @@ build.dist.struct<-
           paste('~',colnames(X)[i], '-1')),data=X)
         X <- cbind(X[,-i], dummyXi)
       }
-      
+
     }else{
       #handle missing data
       for(i in c(1:ncol(X))){
         if(any(is.na(X[,i]))){
           X <- cbind(X,is.na(X[,i]))
           colnames(X)[ncol(X)] <- paste(colnames(X)[i],'NA', sep = '')
-          X[which(is.na(X[,i])),i] <- mean(X[,i], na.rm = TRUE)    
+          X[which(is.na(X[,i])),i] <- mean(X[,i], na.rm = TRUE)
         }
       }
-      
+
     }
-    
-    
-    #get rid of columns that do not vary if the distance measure is Mahalanobis distance 
+
+
+    #get rid of columns that do not vary if the distance measure is Mahalanobis distance
     if(dist.type == "Mahalanobis"){
       varying <- apply(X,2, function(x) length(unique(x)) > 1)
       if(!all(varying) && verbose) print('Constant-value columns found in X, they will not be used to calculate Mahalanobis distance.')
       X <- X[,which(varying),drop = FALSE]
     }
-    
+
     if (calip.option == 'propensity') {
       calip.cov <- glm.fit(cbind(rep(1, nrow(X)),X), z, family = binomial())$linear.predictors
       cal <- sd(calip.cov) * caliper
@@ -95,33 +95,31 @@ build.dist.struct<-
     nums <- 1:nobs
     ctrl.nums <- 1:(sum(z == 0))
     treated <- nums[z == 1]
-    print("line99")
-    
+
     #find distance between each treated and each control it will be connected to and store in a distance structure
     dist.struct <- list()
-    print("line103")
     for (i in c(1:length(treated))) {
       controls <- nums[(z == 0) & (exact == exact[treated[i]])]
       control.names <- ctrl.nums[exact[z == 0] == exact[treated[i]]]
       if(dist.type == "Mahalanobis"){
         costi <- mahalanobis(rX[controls, ,drop=FALSE], rX[treated[i], ], icov, inverted = T)
-      } 
+      }
       if(dist.type == "Euclidean"){
         costi <- rdist(rX[controls, ,drop=FALSE], rX[treated[i], ])
       }
-      
+
       if (calip.option != 'none') {
         calip.update <- rep(0, length(costi))
         calip.update[abs(calip.cov[treated[i]] - calip.cov[controls]) - cal > 0] <- Inf
         costi <- costi + calip.update
       }
       names(costi) <- control.names
-      dist.struct[[i]] <- costi[is.finite(costi)]	
+      dist.struct[[i]] <- costi[is.finite(costi)]
       if(dist.type == "Euclidean"){
         dist.struct[[i]] <- dist.struct[[i]][!is.na(names(dist.struct[[i]]))]
       }
     }
-    
+
     if (sum(laply(dist.struct, length)) == 0) stop('All matches forbidden. Considering using a wider caliper?')
     return(dist.struct)
   }
